2beed8c21bc7000a2082c5776df4591fda56fbfb
Will <70348218+imaginary-png@users.noreply.github.com>
2023-01-22 13:26:13 +1100

    added basic loading and saving of txt file

--- wiki.go
+++ wiki.go
@@ -7,11 +7,26 @@ import (
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
-func (*p Page) save() error {
+func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("Hi, this is a sample page!")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
