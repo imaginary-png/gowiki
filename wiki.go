997666a5e74aaff003d146dc26887947f839ebd6
Will <70348218+imaginary-png@users.noreply.github.com>
2023-01-22 15:37:34 +1100

    added path validation

--- wiki.go
+++ wiki.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"errors"
 )
 
 type Page struct {
@@ -27,27 +29,36 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-	renderTemplate(w, "Templates/view.html", p)
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	renderTemplate(w, "Templates/edit.html", p)
+	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err = p.save()
@@ -58,7 +69,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl)
+	t, err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if !checkForInternalServerError(w,err) {
 		return
 	}
@@ -73,6 +84,19 @@ func checkForInternalServerError(w http.ResponseWriter, error) bool{
 	}
 	return false
 }
+
+func getTitle(w, http.ResponseWriter, r. *http.Request) (string, error){
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil //title is index 2 ?
+}
+
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("Templates/edit.html", "Templates/view.html"))
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
